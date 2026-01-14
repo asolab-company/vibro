939127060b7161827dee89e2454c04fb8a5e3e00
asolab-company <asolab.company@gmail.com>
2026-01-14 21:24:16 +0100

    Add files via upload

---
+++ VibroMate/Constants.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct Constants {
+    static let termsURL = URL(string: "https://docs.google.com/document/d/e/2PACX-1vSh7UiNmaeNGZGaNb9kgVl1sjRll6vlpwUXMRjbfR8l1-VFqSp5IJRFD9Y0GxlHc97LXenB3V4njhbD/pub")!
+    static let privacyURL = URL(string: "https://docs.google.com/document/d/e/2PACX-1vSh7UiNmaeNGZGaNb9kgVl1sjRll6vlpwUXMRjbfR8l1-VFqSp5IJRFD9Y0GxlHc97LXenB3V4njhbD/pub")!
+    static let helpURL = URL(string: "https://docs.google.com/document/d/e/2PACX-1vSh7UiNmaeNGZGaNb9kgVl1sjRll6vlpwUXMRjbfR8l1-VFqSp5IJRFD9Y0GxlHc97LXenB3V4njhbD/pub")!
+
+    static let weekly = "weeklyvibrovibe"
+    static let yearly = "yearlyvibrovibe"
+
+    static let appID = "6749849013"
+}
